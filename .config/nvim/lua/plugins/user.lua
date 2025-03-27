ecee92c7f04c3b16f69142bae3ebca1de7071c00
Joe Maples <joe@maples.dev>
2025-03-26 21:24:11 -0400

    feat(nvim): AstroNvim v5

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -1,4 +1,5 @@
 -- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
 
 ---@type LazySpec
