f31c6bb9a44972a7689c19c7de41b720d79e57ae
Joe Maples <joe@maples.dev>
2025-12-09 15:37:52 -0500

    feat(zellij): Config w/ zellij.vim

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -130,4 +130,15 @@ return {
       },
     },
   },
+  {
+    "https://github.com/fresh2dev/zellij.vim",
+    -- Pin version to avoid breaking changes.
+    -- tag = '0.3.*',
+    lazy = false,
+    init = function()
+      -- Options:
+      -- vim.g.zelli_navigator_move_focus_or_tab = 1
+      -- vim.g.zellij_navigator_no_default_mappings = 1
+    end,
+  },
 }
