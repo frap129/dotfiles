ee58aa5fcad49e1fe57499f4ffd78b3c3249afc5
Joe Maples <joe@maples.dev>
2026-08-05 17:03:52 -0400

    fix: Move some packages back to nix

--- .config/nix-env/flake.nix
+++ .config/nix-env/flake.nix
@@ -8,21 +8,61 @@
       url = "github:jdx/mise/v2026.7.18";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    tirith = {
+      url = "github:sheeki03/tirith";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, mise, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, mise, tirith, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages.default = pkgs.buildEnv {
+        packages.default = with pkgs; buildEnv {
           name = "nix-user-env";
           paths = [
             mise.packages.${system}.default
-            pkgs.yadm
+            tirith.packages.${system}.default
+            apktool
+            bash
+            bash-completion
+            curl
+            dex2jar
+            git
+            gvfs
+            htop
+            less
+            lsof
+            mosh
+            nerd-fonts.fira-code
+            netcat
+            nix-search-cli
+            noto-fonts-color-emoji
+            openssh
+            pigz
+            podman
+            procs
+            rmlint
+            rsync
+            scrcpy
+            sshfs
+            syncthing
+            tailscale
+            ugrep
+            unzip
+            wget
+            which
+            yadm
+            zimfw
+            zip
+            zsh
           ];
-          pathsToLink = [ "/bin" "/share" ];
+          pathsToLink = [ "/bin" "/sbin" "/share" ];
+          postBuild = ''
+            ln -s ${zimfw}/zimfw.zsh $out/share/zimfw.zsh
+          '';
         };
       });
 }
