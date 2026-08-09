294aee14a9a22d69c4e0f1edd1074bf6c52a6142
Joe Maples <joe@maples.dev>
2026-08-09 11:16:34 -0400

    feat(aliases): Add egrep, bunx, fix grep

--- .config/shell/aliases.nu
+++ .config/shell/aliases.nu
@@ -1,13 +1,12 @@
 alias adate = sh "-c" "adb shell date $(date +%m%d%H%M%Y.%S)"
 alias ag = ug "-r" "--ignore-files" "-I" "-n" "--heading" "--break" "--color" "-j" "--sort"
+alias bunx = aubx
 alias cat = bat "-pp"
 alias diff = delta
 alias du = dust
+alias egrep = ugrep "-E"
 alias fhere = find "." "-name"
-alias grep = ugrep
-def --wrapped histg [...rest] {
-  history | grep ...$rest
-}
+alias grep = ugrep "-G"
 def --wrapped myip [...rest] {
   curl "http://ipecho.net/plain" ; echo ...$rest
 }
--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,13 +1,14 @@
 alias adate='sh -c '"'"'adb shell date $(date +%m%d%H%M%Y.%S)'"'"''
 alias ag='ug -r --ignore-files -I -n --heading --break --color -j --sort'
+alias bunx=aubx
 alias cat='bat -pp'
 if command -v delta >/dev/null 2>&1; then
   alias diff=delta
 fi
 alias du=dust
+alias egrep='ugrep -E'
 alias fhere='find . -name'
-alias grep=ugrep
-alias histg='history | grep'
+alias grep='ugrep -G'
 alias myip='curl http://ipecho.net/plain ; echo'
 alias rg='ugrep -r --ignore-files --ignore-files=.ignore --ignore-files=.rgignore -I -n --heading --break --color'
 alias vi=nvim
